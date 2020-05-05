074ab00ccbb050c5610077485d426c27939b28bd
mira <mira.eveliina97@gmail.com>
2020-05-05 17:08:49 +0300

    kokeilu -- uus

--- app/src/main/java/com/example/kouluprojekti/SettingsPage.java
+++ app/src/main/java/com/example/kouluprojekti/SettingsPage.java
@@ -1,4 +1,33 @@
 package com.example.kouluprojekti;
 
+import android.view.View;
+import android.widget.RadioButton;
+
 public class SettingsPage {
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioPurple:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radioBlue:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.radioRed:
+                if (checked)
+                    //
+                    break;
+            case R.id.radioYellow:
+                if (checked)
+                    //
+                    break;
+        }
+    }
+
 }
